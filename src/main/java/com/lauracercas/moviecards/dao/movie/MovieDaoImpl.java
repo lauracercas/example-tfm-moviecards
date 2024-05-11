8c4577683605c4e1cf9f38b8fa2ecc8ee155b869
lauracercas <laura.cercas@edu.uah.es>
2024-05-11 17:07:21 +0200

    fix @Repository

--- src/main/java/com/lauracercas/moviecards/dao/movie/MovieDaoImpl.java
+++ src/main/java/com/lauracercas/moviecards/dao/movie/MovieDaoImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public class MovieDaoImpl implements MovieDao {
 
     private final MovieJPA movieJPA;
--- src/main/java/com/lauracercas/moviecards/repositories/MovieJPA.java
+++ src/main/java/com/lauracercas/moviecards/repositories/MovieJPA.java
@@ -2,7 +2,9 @@ package com.lauracercas.moviecards.repositories;
 
 import com.lauracercas.moviecards.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieJPA extends JpaRepository<Movie, Integer> {
 
 
